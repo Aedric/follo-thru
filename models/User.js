c26842367a33bebcac1b6a564df96b7643d813c0
Aedric Donovan <aedric@Aedrics-MacBook-Air.local>
2018-09-08 12:54:40 -0500

    Profile model, started profile api

--- models/User.js
+++ models/User.js
@@ -21,7 +21,30 @@ const UserSchema = new Schema({
   date: {
     type: Date,
     default: Date.now
-  }
+  },
+  //array of assigned tasks. Might be worth taking updating to task objects, or just worth keeping as an array of unsaturated, making a second call to fill them with all the info
+  tasks: [
+    {
+      id: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      //  The user the task is assigned to
+      assignedId: {
+        type: String,
+        required: true
+      },
+      //  TODO: define and import a group for proper typing. GroupId will do for now. This is the group the task is assigned to.
+      groupId: {
+        type: String,
+        default: ''
+      }
+    }
+  ]
 });
 
 module.exports = Users = mongoose.model('users', UserSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,39 @@
 //  content fields
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//  Load Profile Model
+const Profile = require('../../models/Profile');
+
+//  Load User Profile
+const User = require('../../models/User');
 
 //  @route    GET api/profile/test
 //  @desc     Tests Profile Route
 //  @access   Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile Test Works' }));
+
+//  @route    GET api/profile
+//  @desc     Get Current User's profile
+//  @access   Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
